2f48b21db4423d23f6564650c80512d08f708a2f
Oscar Sanchez <ojsm45@gmail.com>
2020-07-23 01:15:52 -0300

    Add normalize and theme provider config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,30 @@
 import React from "react"
+import Particles from "react-particles-js"
 import { Link } from "gatsby"
-
+import { ThemeProvider } from "styled-components"
+import { Normalize, GlobalStyles } from "../styles/global"
 import Layout from "../components/layout/Layout.component"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import Particles from "react-particles-js"
+
+import { defaultTheme, lightTheme } from "../styles/themes"
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Particles />
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  <ThemeProvider theme={defaultTheme}>
+    <Layout>
+      <SEO title="Home" />
+      <Normalize />
+      <GlobalStyles />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Particles />
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+  </ThemeProvider>
 )
 
 export default IndexPage
---
+++ src/styles/themes.js
@@ -0,0 +1,6 @@
+export const defaultTheme = {
+  primary: "rebeccapurple",
+}
+export const lightTheme = {
+  primary: "rebeccapurple",
+}
